12701ea82304484d969b2e453cfdd04fab0fb651
zhangbin <bin.zhang1@trustway.cn>
2024-09-24 17:36:30 +0800

    更新

--- script.js
+++ script.js
@@ -4,15 +4,23 @@ function convertImageToBase64() {
     reader.onload = function(e) {
         document.getElementById('base64Output').value = e.target.result;
     };
-    reader.readAsDataURL(file);
+    if (file) {
+        reader.readAsDataURL(file);
+    } else {
+        alert("请先选择一张图片！");
+    }
 }
 
 function convertBase64ToImage() {
-    const base64String = document.getElementById('base64Input').value;
-    const img = new Image();
-    img.src = base64String;
-    document.getElementById('imageOutput').innerHTML = ''; // 清空上次的输出
-    document.getElementById('imageOutput').appendChild(img);
+    const base64String = document.getElementById('base64Input').value.trim();
+    if (base64String) {
+        const img = new Image();
+        img.src = base64String;
+        document.getElementById('imageOutput').innerHTML = ''; // 清空上次的输出
+        document.getElementById('imageOutput').appendChild(img);
+    } else {
+        alert("请先输入Base64字符串！");
+    }
 }
 
 function changeLanguage() {
